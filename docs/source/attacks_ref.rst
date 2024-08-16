4ab5255f6eab36a88e0cfdffd010d34d770b6684
Salah GHAMIZI <salah.ghamizi@gmail.com>
2024-08-16 11:19:59 +0200

    Added autodoc for attacks

---
+++ docs/source/attacks_ref.rst
@@ -0,0 +1,8 @@
+.. automodule:: tabularbench.attacks.capgd.capgd
+    :members:
+
+.. automodule:: tabularbench.attacks.moeva.moeva
+    :members:
+
+.. automodule:: tabularbench.attacks.caa.caa
+    :members:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,8 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os, sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = "TabularBench"
 copyright = "2024, Thibault Simonetto, Salah Ghamizi, Maxime Cordy"
@@ -14,8 +16,15 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = ['myst_parser',  'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',]
 
+autosummary_generate = True
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
